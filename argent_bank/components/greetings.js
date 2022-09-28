4e87370db40b5a7165597454bbff3c76587ac8cd
Kollmann7 <82177505+Kollmann7@users.noreply.github.com>
2022-09-28 16:34:09 +0200

    add logout

--- argent_bank/components/greetings.js
+++ argent_bank/components/greetings.js
@@ -1,5 +1,4 @@
 import styles from '../styles/Greetings.module.css'
-import axios from 'axios'
 import { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { selecedtUser, displayUser } from '../features/userSlice'
@@ -54,9 +53,9 @@ export default function Greetings () {
                         <div className={styles.wrapper}>
                             <label for="LastName">Last name</label><input type="text" id="username" value={newLastName} onChange={(e) => setNewLastName(e.target.value)}/>
                         </div>
-                        <div>
-                        <button className={styles.button} onClick={handleUpdate}>Update name</button>
-                        <button className={styles.button} onClick={handleCancel}>Cancel</button>
+                        <div className={styles.buttonWrapper}>
+                            <button className={styles.button} onClick={handleUpdate}>Update name</button>
+                            <button className={styles.button} onClick={handleCancel}>Cancel</button>
                         </div>
                     </form>
                 )}
--- argent_bank/components/signOut.js
+++ argent_bank/components/signOut.js
@@ -1,15 +1,27 @@
 import Link from 'next/link'
 import styles from '../styles/Header.module.css'
+import { useSelector, useDispatch } from "react-redux"
+import { logout , selecedtUser} from '../features/userSlice'
 
 export default function SignOut () {
+    const user = useSelector(selecedtUser)
+    const dispatch = useDispatch()
+
+    const handleLogout = () => {
+        dispatch(logout())
+    }
     return(
         <>
         <div className={styles.user}>
             <Link className={styles.item} href="/user">
-            Tony
+                <div>
+                    {user.firstName}
+                </div>
             </Link>
-            <Link className={styles.item} href="/">
-            Sign Out
+            <Link className={styles.item} href="/" >
+                <button className={styles.button} onClick={handleLogout}>
+                    Sign Out
+                </button>
             </Link>
         </div>
         </>

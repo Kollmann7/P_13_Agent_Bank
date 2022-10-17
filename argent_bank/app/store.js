d40759ee884097e588f33d6de75abec4a509354d
Kollmann7 <82177505+Kollmann7@users.noreply.github.com>
2022-10-17 17:53:16 +0200

    add user ID

--- argent_bank/app/store.js
+++ argent_bank/app/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { userSlice } from '../features/userSlice'
+import { HYDRATE } from 'next-redux-wrapper'
 
 export default configureStore({
   reducer: {
@@ -7,3 +8,18 @@ export default configureStore({
   }
 })
 
+// const masterReducer = (state, action) => {
+//   if(action.type === HYDRATE) {
+//     const nextState = {
+//       ...state,
+//       counter: {
+//         user: state.counter.user + action.payload.counter.user,
+//       }
+//     }
+//     return nextState
+//   } else {
+//     return configureStore
+//   }
+// }
+
+// export const wrapper = createWrapper()
--- argent_bank/pages/user/[id].js
+++ argent_bank/pages/user/[id].js
@@ -4,21 +4,48 @@ import SignOutHeader from '../../components/signOutHeader'
 import styles from '../../styles/User.module.css'
 import Greetings from '../../components/greetings'
 import Account from '../../components/account'
+import { useDispatch, useSelector } from 'react-redux'
+import { selecedtUser } from '../../features/userSlice'
+import { setUser } from '../../features/userSlice'
+import api from '../api/api'
 
 
-export default function User (){
-    return(
+export default function User ({data}){
+    useSelector(selecedtUser)
+    const dispatch = useDispatch()
+    dispatch(setUser(data.body))
+
+    return (
         <>
-        <Head>
-            <title>Argent Bank</title>
-            
-        </Head>
-        <SignOutHeader />
-        <main className={styles.dark}>
-            <Greetings />
-            <Account />
-        </main>
-        <Footer />
+            <Head>
+                <title>Argent Bank</title>
+            </Head>
+            <SignOutHeader />
+            <main className={styles.dark}>
+                <Greetings />
+                <Account />
+            </main>
+            <Footer />
         </>
     )
+}
+export async function getServerSideProps({req, res}) {
+    const token = req.cookies.token
+    api.defaults.headers.Authorization = `Bearer ${token}`
+    let {data} =  {}
+    if(!!token){
+      ({data} =  await api.get('/user/profile'))
+    }
+
+    if(token === undefined){
+        return {
+            redirect:{
+            destination: '/',
+            }
+        }
+    }
+
+    return {
+      props:{ data : !!token ? data : null }
+    }
 }
